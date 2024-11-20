a5aa80b9d37902c7895c76cb72d583b2e963b32a
renekrewinkel <rene@krewinkel.nl>
2024-11-20 15:23:56 +0100

    Initial

---
+++ .storybook/preview.js
@@ -0,0 +1,15 @@
+/** @type { import('@storybook/react').Preview } */
+import '../src/resources/styles/main.css'
+
+const preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
